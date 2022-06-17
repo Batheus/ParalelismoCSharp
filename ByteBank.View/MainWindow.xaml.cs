987d1c17b723a99eb888ce087678932e5b93e539
Batheus <batheus.dev@gmail.com>
2022-06-16 22:49:26 -0300

    Adicionar arquivos de projeto.

---
+++ ByteBank.View/MainWindow.xaml.cs
@@ -0,0 +1,155 @@
+﻿using ByteBank.Core.Model;
+using ByteBank.Core.Repository;
+using ByteBank.Core.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ByteBank.View
+{
+    public partial class MainWindow : Window
+    {
+        private readonly ContaClienteRepository r_Repositorio;
+        private readonly ContaClienteService r_Servico;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            r_Repositorio = new ContaClienteRepository();
+            r_Servico = new ContaClienteService();
+        }
+
+        private void BtnProcessar_Click(object sender, RoutedEventArgs e)
+        {
+            var contas = r_Repositorio.GetContaClientes();
+
+            var contasQuantidadePorThread = contas.Count() / 8;
+
+            var contas_parte1 = contas.Take(contasQuantidadePorThread);
+            var contas_parte2 = contas.Skip(contasQuantidadePorThread).Take(contasQuantidadePorThread);
+            var contas_parte3 = contas.Skip(contasQuantidadePorThread * 2).Take(contasQuantidadePorThread);
+            var contas_parte4 = contas.Skip(contasQuantidadePorThread * 3).Take(contasQuantidadePorThread);
+            var contas_parte5 = contas.Skip(contasQuantidadePorThread * 4).Take(contasQuantidadePorThread);
+            var contas_parte6 = contas.Skip(contasQuantidadePorThread * 5).Take(contasQuantidadePorThread);
+            var contas_parte7 = contas.Skip(contasQuantidadePorThread * 6).Take(contasQuantidadePorThread);
+            var contas_parte8 = contas.Skip(contasQuantidadePorThread * 7);
+
+            var resultado = new List<string>();
+
+            AtualizarView(new List<string>(), TimeSpan.Zero);
+
+            var inicio = DateTime.Now;
+
+            Thread thread_parte1 = new Thread(() =>
+            {
+                foreach (var conta in contas_parte1)
+                {
+                    var resultadoProcessamento = r_Servico.ConsolidarMovimentacao(conta);
+                    resultado.Add(resultadoProcessamento);
+                }
+            });
+            Thread thread_parte2 = new Thread(() =>
+            {
+                foreach (var conta in contas_parte2)
+                {
+                    var resultadoProcessamento = r_Servico.ConsolidarMovimentacao(conta);
+                    resultado.Add(resultadoProcessamento);
+                }
+            });
+            Thread thread_parte3 = new Thread(() =>
+            {
+                foreach (var conta in contas_parte3)
+                {
+                    var resultadoProcessamento = r_Servico.ConsolidarMovimentacao(conta);
+                    resultado.Add(resultadoProcessamento);
+                }
+            });
+            Thread thread_parte4 = new Thread(() =>
+            {
+                foreach (var conta in contas_parte4)
+                {
+                    var resultadoProcessamento = r_Servico.ConsolidarMovimentacao(conta);
+                    resultado.Add(resultadoProcessamento);
+                }
+            });
+            Thread thread_parte5 = new Thread(() =>
+            {
+                foreach (var conta in contas_parte4)
+                {
+                    var resultadoProcessamento = r_Servico.ConsolidarMovimentacao(conta);
+                    resultado.Add(resultadoProcessamento);
+                }
+            });
+            Thread thread_parte6 = new Thread(() =>
+            {
+                foreach (var conta in contas_parte4)
+                {
+                    var resultadoProcessamento = r_Servico.ConsolidarMovimentacao(conta);
+                    resultado.Add(resultadoProcessamento);
+                }
+            });
+            Thread thread_parte7 = new Thread(() =>
+            {
+                foreach (var conta in contas_parte4)
+                {
+                    var resultadoProcessamento = r_Servico.ConsolidarMovimentacao(conta);
+                    resultado.Add(resultadoProcessamento);
+                }
+            });
+            Thread thread_parte8 = new Thread(() =>
+            {
+                foreach (var conta in contas_parte4)
+                {
+                    var resultadoProcessamento = r_Servico.ConsolidarMovimentacao(conta);
+                    resultado.Add(resultadoProcessamento);
+                }
+            });
+
+            thread_parte1.Start();
+            thread_parte2.Start();
+            thread_parte3.Start();
+            thread_parte4.Start();
+            thread_parte5.Start();
+            thread_parte6.Start();
+            thread_parte7.Start();
+            thread_parte8.Start();
+
+            while (thread_parte1.IsAlive || thread_parte2.IsAlive
+                || thread_parte3.IsAlive || thread_parte4.IsAlive 
+                || thread_parte5.IsAlive || thread_parte6.IsAlive
+                || thread_parte7.IsAlive || thread_parte8.IsAlive)
+            {
+                Thread.Sleep(250);
+                //Programa não faz nada enquanto as threads estão em operação
+            }
+
+            var fim = DateTime.Now;
+
+            AtualizarView(resultado, fim - inicio);
+
+            //Programa finaliza o processamento em 2.770 segundos
+        }
+
+        private void AtualizarView(List<String> result, TimeSpan elapsedTime)
+        {
+            var tempoDecorrido = $"{elapsedTime.Seconds}.{elapsedTime.Milliseconds} segundos!";
+            var mensagem = $"Processamento de {result.Count} clientes em {tempoDecorrido}";
+
+            LstResultados.ItemsSource = result;
+            TxtTempo.Text = mensagem;
+        }
+    }
+}
